c2c67a893700f45c31f19532c3977d464c47ef8b
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-08-31 15:26:27 +0200

    done with fetch

--- src/apis/stocksApi.js
+++ src/apis/stocksApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const stocksUrl = 'https://financialmodelingprep.com/api/v3/actives?apikey=61d3e77e74207aa24ad5a8c19e3746ab';
+const stocksUrl = 'https://financialmodelingprep.com/api/v3/actives?apikey=cfb96111e5b3134b48b788d8f8bdb079';
 
 const getStocksData = async () => {
   try {
