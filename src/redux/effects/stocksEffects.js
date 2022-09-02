ffd300093f17d204715482c0abceb4936e695291
rotshidzw <69056906+rotshidzw@users.noreply.github.com>
2022-09-02 13:16:50 +0200

    adding style

--- src/redux/effects/stocksEffects.js
+++
@@ -1,8 +0,0 @@
-const stocksEffectCleanData = ({ payload }) => payload.map((companyStock) => ({
-  companyName: companyStock.companyName,
-  symbol: companyStock.ticker,
-  stockPrice: companyStock.price,
-  changes: companyStock.changes,
-}));
-
-export default stocksEffectCleanData;
